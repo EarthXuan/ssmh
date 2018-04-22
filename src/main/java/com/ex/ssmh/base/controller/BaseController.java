24d8fc1b7fca61e6841deb1e2b6e68c1f16f1a05
EarthXuan <836569303@qq.com>
2018-04-22 14:53:06 +0800

    SSMH项目搭建-spring+spirngmvc+mybatis+hibernate

---
+++ src/main/java/com/ex/ssmh/base/controller/BaseController.java
@@ -0,0 +1,20 @@
+package com.ex.ssmh.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/** * 
+*@author  EarthXuan 
+*@date 创建时间：2018年4月15日 下午9:48:25
+*@version 1.0
+*/
+@Controller
+@RequestMapping("base")
+public class BaseController {
+	
+	public String test()
+	{
+		return "";
+	}
+}
